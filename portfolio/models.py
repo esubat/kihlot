ade93c0676c91920dca596e7caf3533400d28b23
esuba <esuba419@gmail.com>
2024-07-31 10:48:42 +0300

    added Profile, Project, ProjectImage, Experience  models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    # the title field in here is a specific role or designation of a user .
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    title = models.CharField(max_length=100, null=True, blank=True)
+    image = models.ImageField(upload_to='userProfile/', null=True)
+    summary = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Project(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects')
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=250, 
+                                   null=True,
+                                   blank=True)
+
+    def __str__(self):
+        return self.title
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='project_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Image for {self.project.title if self.project else 'unknown Project'}"
+
+class Experience(models.Model):
+
+    profile = models.ForeignKey(Profile, 
+                                on_delete=models.CASCADE,
+                                related_name='experiences')
+    title = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} at {self.company}"
