afbb665851663e289eeda1ccb83c210f92e6282b
esuba <esuba419@gmail.com>
2024-07-31 11:02:20 +0300

    Alter field end_date on experience

--- portfolio/models.py
+++ portfolio/models.py
@@ -38,7 +38,8 @@ class Experience(models.Model):
     title = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
     start_date = models.DateField()
-    end_date = models.DateField(null=True)
+    end_date = models.DateField(null=True,
+                                blank=True)
 
     def __str__(self):
         return f"{self.title} at {self.company}"
