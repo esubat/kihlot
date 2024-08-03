732eba99844d55fcabb044fc98f34e1af5b87107
esuba <esuba419@gmail.com>
2024-08-04 00:53:15 +0300

    url for user update view

--- portfolio/api/urls.py
+++ portfolio/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, UserDetailView
+from .views import RegisterView, LoginView, LogoutView, UserDetailView,UserProfileUpdateView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='api_login'),
     path('profile/<str:username>/', UserDetailView.as_view(), name='user_detail'),
+    path('update/',UserProfileUpdateView.as_view() , name ='profile_update'),
+    path('login/', LoginView.as_view(), name='api_login'),
     path('logout/', LogoutView.as_view(), name='api_logout'),
 ]
